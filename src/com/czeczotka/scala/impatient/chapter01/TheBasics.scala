a05ca042f95bd5634e4e6c8d155deac1cfb8d334
Jakub Czeczótka <jakub.czeczotka@gmail.com>
2015-04-09 23:08:30 +0100

    First commit for chapter 1 - The Basics

---
+++ src/com/czeczotka/scala/impatient/chapter01/TheBasics.scala
@@ -0,0 +1,11 @@
+object TheBasics {
+  
+  def exercise03() {
+    // REPL results are values
+  }
+  
+  def exercise04() {
+    // it calls the StringOps.*(3) method
+    // scala.collection.immutable.StringOps.*(n: Int): String 
+  }
+}
