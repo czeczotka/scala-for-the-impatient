4490a35b51ef6c2a5abbae0e9df5f7e19a00b6fb
Jakub Czeczótka <jakub.czeczotka@gmail.com>
2015-04-09 23:53:55 +0100

    Add exercises 5 and 6 to The Basics

--- src/com/czeczotka/scala/impatient/chapter01/TheBasics.scala
+++ src/com/czeczotka/scala/impatient/chapter01/TheBasics.scala
@@ -1,4 +1,10 @@
+package com.czeczotka.scala.impatient.chapter01
+
 object TheBasics {
+  
+  def main(args: Array[String]) {
+    exercise06()
+  }
   
   def exercise03() {
     // REPL results are values
@@ -7,5 +13,15 @@ object TheBasics {
   def exercise04() {
     // it calls the StringOps.*(3) method
     // scala.collection.immutable.StringOps.*(n: Int): String 
+  }
+  
+  def exercise05() {
+    // scala.runtime.RichInt.max(that: Int): Int 
+  }
+  
+  def exercise06() {
+    val two: BigInt = 2
+    val result = two.pow(1024)
+    println(result)
   }
 }
