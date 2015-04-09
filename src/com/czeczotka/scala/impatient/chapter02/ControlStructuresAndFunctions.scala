bd6163eb908d2a243c9f5453a2c1aeb65dfa4909
Jakub Czeczótka <jakub.czeczotka@gmail.com>
2015-04-10 00:23:00 +0100

    Add exercises 2, 3, 4 and 5 to Control Structures And Functions

--- src/com/czeczotka/scala/impatient/chapter02/ControlStructuresAndFunctions.scala
+++ src/com/czeczotka/scala/impatient/chapter02/ControlStructuresAndFunctions.scala
@@ -4,6 +4,8 @@ object ControlStructuresAndFunctions {
   
   def main(args: Array[String]) {
     exercise01()
+    exercise04()
+    exercise05()
   }
   
   def exercise01() {
@@ -17,6 +19,30 @@ object ControlStructuresAndFunctions {
     if (x > 0) result = 1
     else if (x < 0) result = -1
     result
+  }
+  
+  def exercise02() {
+    // Unit?
+  }
+  
+  def exercise03() {
+    // you want x to be of type Unit
+    // var x: Any = null
+    // var y: Int = 1
+  }
+  
+  def exercise04() {
+    for (i <- (0 to 10).reverse) print(i + " ")
+    println
+  }
+  
+  def exercise05() {
+    countdown(20)
+  }
+  
+  def countdown(n: Int) {
+    for (i <- (0 to n).reverse) 
+      print(i + " ")
   }
   
 }
