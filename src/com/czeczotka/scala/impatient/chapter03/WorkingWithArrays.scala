402e231074624606a63ea9359466ed952309add8
Jakub Czeczótka <jakub.czeczotka@gmail.com>
2015-04-10 22:52:39 +0100

    Add exercise 4 and 5 to Working with Arrays

--- src/com/czeczotka/scala/impatient/chapter03/WorkingWithArrays.scala
+++ src/com/czeczotka/scala/impatient/chapter03/WorkingWithArrays.scala
@@ -6,22 +6,25 @@ object WorkingWithArrays {
     exercise01()
     exercise02()
     exercise03()
+    exercise04()
+    exercise05()
   }
   
   def exercise01() {
-    val array = getIntArray(10, 10)
+    val array = getPositiveIntArray(10, 10)
     println(array.toBuffer)
   }
   
-  def getIntArray(size: Int, range: Int): Array[Int] = {
+  def getPositiveIntArray(size: Int, range: Int): Array[Int] = {
     return Array.fill(size)(new java.util.Random().nextInt(range))
   }
   
   def exercise02() {
-    val array = getIntArray(5, 10)
+    val array = getPositiveIntArray(5, 10)
     println("Before: " + array.toBuffer)
     swapAdjacentElements(array)
     println("After:  " + array.toBuffer)
+    println
   }
   
   def swapAdjacentElements(array: Array[Int]) {
@@ -35,10 +38,11 @@ object WorkingWithArrays {
   }
   
   def exercise03() {
-    val array = getIntArray(5, 10)
+    val array = getPositiveIntArray(5, 10)
     println("Before: " + array.toBuffer)
     val swappedArray = arrayWithSwappedAdjacentElements(array)
     println("After:  " + swappedArray.toBuffer)
+    println
   }
   
   def arrayWithSwappedAdjacentElements(array: Array[Int]) = {
@@ -54,4 +58,36 @@ object WorkingWithArrays {
         array(i)
       }
   }
+  
+  def exercise04() {
+    val array = getIntArray(5, 10)
+    println("Before: " + array.toBuffer)
+    val filteredArray = arrayWithFilteredElements(array)
+    println("After:  " + filteredArray.toBuffer)
+    println
+  }
+  
+  def getIntArray(size: Int, range: Int): Array[Int] = {
+    return Array.fill(size)(new java.util.Random().nextInt() % range)
+  }
+  
+  def arrayWithFilteredElements(array: Array[Int]) = {
+    array.filter(_ > 0) ++ array.filter(_ <= 0) 
+  }
+  
+  def exercise05() {
+    val array = getDoubleArray(10)
+    println("Double array:  " + array.toBuffer)
+    val average: Double = averageValue(array)
+    println("Average value: " + average)
+    println
+  }
+  
+  def getDoubleArray(size: Int): Array[Double] = {
+    return Array.fill(size)(new java.util.Random().nextDouble())
+  }
+  
+  def averageValue(array: Array[Double]): Double = {
+    array.sum.toDouble / array.length.toDouble
+  } 
 }
