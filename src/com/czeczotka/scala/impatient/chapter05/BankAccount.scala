9c7b8c40450e1f8ea6f4ae20913766f859ff2df5
Jakub Czeczótka <jakub.czeczotka@gmail.com>
2015-04-11 23:51:16 +0100

    Add exercise 2 to Classes

---
+++ src/com/czeczotka/scala/impatient/chapter05/BankAccount.scala
@@ -0,0 +1,19 @@
+package com.czeczotka.scala.impatient.chapter05
+
+class BankAccount {
+  
+  private var balance = 0.0
+  
+  def deposit(amount: Double) {
+    balance += amount
+  }
+  
+  def withdraw(amount: Double) {
+    balance -= amount
+  }
+  
+  def accountBalance(): Double = {
+    balance
+  }
+
+}
--- src/com/czeczotka/scala/impatient/chapter05/Classes.scala
+++ src/com/czeczotka/scala/impatient/chapter05/Classes.scala
@@ -3,15 +3,23 @@ package com.czeczotka.scala.impatient.chapter05
 object Classes {
   
   def main(args: Array[String]) {
-    exercise01()
+    exercise01_counter()
+    exercise02_bankAccount()
   }
 
-  def exercise01() {
+  def exercise01_counter() {
     val c = new CounterImproved()
     c.value = Int.MaxValue
     println("Counter current " + c.current())
     c.increment()
     println("Counter current " + c.current())
+  }
+  
+  def exercise02_bankAccount() {
+    val ba = new BankAccount()
+    ba.deposit(100.0)
+    ba.withdraw(80.0)
+    println("Bank account balance " + ba.accountBalance())
   }
   
 }
