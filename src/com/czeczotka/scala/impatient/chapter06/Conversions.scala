4d29589a8f9c682831eeafac53232e87a09714e7
Jakub Czeczótka <jakub.czeczotka@gmail.com>
2015-04-13 23:22:38 +0100

    Add exercise 1 to Objects

---
+++ src/com/czeczotka/scala/impatient/chapter06/Conversions.scala
@@ -0,0 +1,17 @@
+package com.czeczotka.scala.impatient.chapter06
+
+object Conversions {
+  
+  def inchesToCentimeters(inches: Double): Double = {
+    inches * 2.54
+  }
+  
+  def gallonsToLiters(gallons: Double): Double = {
+    gallons * 4.54609
+  }
+  
+  def milesToKilometers(miles: Double): Double = {
+    miles * 1.609344
+  }
+
+}
--- src/com/czeczotka/scala/impatient/chapter06/Objects.scala
+++ src/com/czeczotka/scala/impatient/chapter06/Objects.scala
@@ -3,9 +3,20 @@ package com.czeczotka.scala.impatient.chapter06
 object Objects {
   
   def main(args: Array[String]) {
+    exercise01_conversions()
     exercise04_point()
   }
   
+  def exercise01_conversions() {
+    val inches = 12
+    val gallons = 5
+    val miles = 10
+    println("%d inches equals %f centimeters".format(inches, Conversions.inchesToCentimeters(inches)))
+    println("%d gallons equals %f litres".format(gallons, Conversions.gallonsToLiters(gallons)))
+    println("%d miles equals %f kilometers".format(miles, Conversions.milesToKilometers(miles)))
+    println
+  }
+    
   def exercise04_point() {
     val p = Point(3, 4)
     println(p.toString())
