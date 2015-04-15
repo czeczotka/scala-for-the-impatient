c83f9532e931e933b262a72bb3b7396a74041e1a
Jakub Czeczótka <jakub.czeczotka@gmail.com>
2015-04-15 07:43:36 +0100

    Add PackagesAndImports object for chapter 7

---
+++ src/com/czeczotka/scala/impatient/chapter07/PackagesAndImports.scala
@@ -0,0 +1,13 @@
+package com.czeczotka.scala.impatient.chapter07
+
+object PackagesAndImports {
+  
+  def main(args: Array[String]) {
+    exercise01()
+  } 
+  
+  def exercise01() {
+    
+  }
+
+}
