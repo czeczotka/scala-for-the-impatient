0a3a468c7bf530808be9053522f5c9160a789051
Jakub Czeczótka <jakub.czeczotka@gmail.com>
2015-04-18 23:43:50 +0100

    Add exercise 2 to Files and Regular Expressions

--- src/com/czeczotka/scala/impatient/chapter09/FilesAndRegularExpressions.scala
+++ src/com/czeczotka/scala/impatient/chapter09/FilesAndRegularExpressions.scala
@@ -6,21 +6,46 @@ import scala.io.Source
 
 object FilesAndRegularExpressions {
 
-  val FILE = "moby-dick.txt"
-  val OUTPUT = "moby-dick-reversed.txt"
+  val SPACES_FOR_TAB = "  "
   val SRC = "src"
-  val FILE_PATH = SRC + System.getProperty("file.separator") + FILE
-  val OUTPUT_PATH = SRC + System.getProperty("file.separator") + OUTPUT
+  
+  val EX1_FILE = "moby-dick.txt"
+  val EX1_OUTPUT = "moby-dick-reversed.txt"
+  val EX1_FILE_PATH = SRC + System.getProperty("file.separator") + EX1_FILE
+  val EX1_OUTPUT_PATH = SRC + System.getProperty("file.separator") + EX1_OUTPUT
+
+  val EX2_FILE = "moby-dick-copy.txt"
+  val EX2_FILE_PATH = SRC + System.getProperty("file.separator") + EX2_FILE
+
 
   def main(args: Array[String]) {
     exercise01_reverseLines()
+    exercise02_tabs()
   }
 
   def exercise01_reverseLines() {
-    val in = Source.fromFile(FILE_PATH, "UTF-8")
+    val in = Source.fromFile(EX1_FILE_PATH, "UTF-8")
     val reversedLines = in.getLines().toBuffer.reverse
-    val out = new PrintWriter(OUTPUT_PATH)
+    in.close()
+    
+    val out = new PrintWriter(EX1_OUTPUT_PATH)
     for (line <- reversedLines) out.println(line)
+    out.close()
+  }
+  
+  def exercise02_tabs() {
+    val in = Source.fromFile(EX2_FILE_PATH, "UTF-8")
+    val inputString = in.mkString
+    in.close()
+    
+    val out = new PrintWriter(EX2_FILE_PATH)
+    for (character <- inputString) {
+      if (character == '\t') {
+        out.print(SPACES_FOR_TAB)
+      } else {
+        out.print(character)
+      }
+    }
     out.close()
   }
 }
