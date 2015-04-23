a60753c832ad090513c343dba78230d149c37c5d
Jakub Czeczótka <jakub.czeczotka@gmail.com>
2015-04-23 23:34:58 +0100

    Clean up Person.scala

--- src/com/czeczotka/scala/impatient/chapter09/exercise/Person.scala
+++ src/com/czeczotka/scala/impatient/chapter09/exercise/Person.scala
@@ -6,14 +6,6 @@ class Person(val name: String) extends Serializable {
   var friends: List[Person] = List[Person]()
   
   override def toString() = {
-//    val friendNames = " -> friends: " +  {
-//      if (friends.size > 0)
-//        for(friend <- friends) yield friend.name
-//      else {
-//        ""
-//      }
-//    }
-    
     val friendNames = if (!friends.isEmpty) {
       " -> friends: " + friends.map(_.name) 
     }  else {
