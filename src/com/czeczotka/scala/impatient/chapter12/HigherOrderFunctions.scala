c1107d36fcdf64f120532b0f30fd6f7b3a19468e
Jakub Czeczótka <jakub.czeczotka@gmail.com>
2015-06-03 20:24:06 +0100

    Add exercise 2 to HigherOrderFunctions

--- src/com/czeczotka/scala/impatient/chapter12/HigherOrderFunctions.scala
+++ src/com/czeczotka/scala/impatient/chapter12/HigherOrderFunctions.scala
@@ -4,6 +4,7 @@ object HigherOrderFunctions {
 
   def main(args: Array[String]) {
     exercise01_values()
+    exercise02_reduceLeft()
   }
 
   def exercise01_values() = {
@@ -12,6 +13,16 @@ object HigherOrderFunctions {
     val myValues = values((x: Int) => x * x, -5, 5)
     myValues.foreach(print)
     println()
+  }
+
+  def exercise02_reduceLeft() {
+    println("EXERCISE 2: largest array element with reduceLeft")
+
+    val array = Array(2, 4, -5, 7, 0, 9, 1, -7)
+    val largest = array.reduceLeft(_.max(_))
+
+    println(s"The largest element in ${array.toBuffer} is $largest")
+    println
   }
 
   def values(fun: (Int) => Int, low: Int, high: Int): Seq[(Int, Int)] = {
