38a24599ed80d2b67356e2307fb01279af82e2b0
Jakub Czeczótka <jakub.czeczotka@gmail.com>
2015-07-07 22:09:26 +0100

    Add exercise 3 to PatternMatchingAndCaseClasses

--- src/com/czeczotka/scala/impatient/chapter14/PatternMatchingAndCaseClasses.scala
+++ src/com/czeczotka/scala/impatient/chapter14/PatternMatchingAndCaseClasses.scala
@@ -5,6 +5,7 @@ object PatternMatchingAndCaseClasses {
   def main(args: Array[String]) {
     exercise01_jdkFallsThrough()
     exercise02_swap()
+    exercise03_swapArray()
   }
 
   def exercise01_jdkFallsThrough() {
@@ -16,12 +17,31 @@ object PatternMatchingAndCaseClasses {
     val second = 5
     println("EXERCISE 2: swap a pair of integers")
     println(s"swap($first, $second) = ${swap(first, second)}")
+    println()
   }
 
-  def swap(first: Int, second: Int): (Int, Int) = {
+  def exercise03_swapArray() {
+    def printLine(array: Array[Int]) {
+      println(s"swapArray(${array.mkString(",")}) = ${swapArray(array).mkString(",")}")
+    }
+    println("EXERCISE 3: swap two first elements in an array")
+    printLine(Array(1, 2, 3, 4, 5, 6, 7))
+    printLine(Array(1, 2, 3))
+    printLine(Array(5, 8))
+    printLine(Array(1))
+    printLine(Array())
+    println()
+  }
+
+  def swapArray(array: Array[Int]): Array[Int] =
+    array match {
+      case Array(first, second, _*) => Array(second, first) ++ array.tail.tail
+      case _ => array
+    }
+
+  def swap(first: Int, second: Int): (Int, Int) =
     (first, second) match {
       case _ => (second, first)
     }
-  }
 
 }
