b5d9663a4c082d5b69d8b21a5baae622d1706269
Jakub Czeczótka <jakub.czeczotka@gmail.com>
2015-09-01 22:03:44 +0100

    Add exercise 3 to TypeParameters

--- src/com/czeczotka/scala/impatient/chapter17/TypeParameters.scala
+++ src/com/czeczotka/scala/impatient/chapter17/TypeParameters.scala
@@ -5,14 +5,15 @@ object TypeParameters {
   def main(args: Array[String]) {
     exercise01_pairSwap()
     exercise02_pairSwap()
+    exercise03_pairSwap()
   }
 
   def exercise01_pairSwap() {
-    case class Pair[A, B](first: A, second: B) {
-      def swap(): Pair[B, A] = new Pair(second, first)
+    case class Pair[T, S](first: T, second: S) {
+      def swap(): Pair[S, T] = new Pair(second, first)
     }
     val pair = new Pair(1, "one")
-    println("EXERCISE 1: Pair swap")
+    println("EXERCISE 1: Pair swap for Pair[T, S]")
     println(s"Pair: $pair, swapped: ${pair.swap()}")
     println()
   }
@@ -25,11 +26,25 @@ object TypeParameters {
         this.second = tmp
       }
     }
-    val pair = new Pair(1, 17)
-    println("EXERCISE 2: Pair swap")
+    val pair = new Pair(2, 22)
+    println("EXERCISE 2: Pair swap for Pair[T]")
     println(s"Pair: $pair")
     pair.swap()
-    println(s"swapped: ${pair}")
+    println(s"swapped: $pair")
+    println()
+  }
+
+  def exercise03_pairSwap() {
+
+    case class Pair[T, S](first: T, second: S)
+
+    def swap[A, B](pair: Pair[A, B]): Pair[B, A] = {
+      Pair(pair.second, pair.first)
+    }
+
+    val pair = new Pair(3, "three")
+    println("EXERCISE 3: Pair swap for generic swap method")
+    println(s"Pair: $pair, swapped: ${swap(pair)}")
     println()
   }
 }
