a755cc43fc1284474a5af1ae0e642128d4e02910
Jakub Czeczótka <jakub.czeczotka@gmail.com>
2015-09-01 21:46:08 +0100

    Add exercise 2 to TypeParameters

--- src/com/czeczotka/scala/impatient/chapter17/TypeParameters.scala
+++ src/com/czeczotka/scala/impatient/chapter17/TypeParameters.scala
@@ -4,6 +4,7 @@ object TypeParameters {
 
   def main(args: Array[String]) {
     exercise01_pairSwap()
+    exercise02_pairSwap()
   }
 
   def exercise01_pairSwap() {
@@ -12,7 +13,23 @@ object TypeParameters {
     }
     val pair = new Pair(1, "one")
     println("EXERCISE 1: Pair swap")
-    println(s"Pair: $pair, swapped ${pair.swap()}")
+    println(s"Pair: $pair, swapped: ${pair.swap()}")
+    println()
+  }
+
+  def exercise02_pairSwap() {
+    case class Pair[T](var first: T, var second: T) {
+      def swap() {
+        val tmp = first
+        this.first = second
+        this.second = tmp
+      }
+    }
+    val pair = new Pair(1, 17)
+    println("EXERCISE 2: Pair swap")
+    println(s"Pair: $pair")
+    pair.swap()
+    println(s"swapped: ${pair}")
     println()
   }
 }
