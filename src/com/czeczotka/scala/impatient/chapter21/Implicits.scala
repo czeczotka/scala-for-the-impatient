a722358ccf60cbd70a0608640bc1ed3e20a4af47
Jakub Czeczótka <jakub.czeczotka@gmail.com>
2015-12-08 21:58:03 +0000

    Add exercise 5 to Implicits

--- src/com/czeczotka/scala/impatient/chapter21/Implicits.scala
+++ src/com/czeczotka/scala/impatient/chapter21/Implicits.scala
@@ -1,6 +1,7 @@
 package com.czeczotka.scala.impatient.chapter21
 
 import com.czeczotka.scala.impatient.chapter21.exercise.Fraction
+import com.czeczotka.scala.impatient.chapter21.exercise.Fraction.smaller
 import com.czeczotka.scala.impatient.chapter21.exercise.MyRichInt.int2MyRichInt
 
 object Implicits extends App {
@@ -8,6 +9,7 @@ object Implicits extends App {
   fractions()
   exercise02_plusPercentageOperator()
   exercise03_factorial()
+  exercise05_implicitConversionsWithImplicitParameters()
 
   def fractions() {
     val result = 3 * Fraction(4, 5)
@@ -27,6 +29,14 @@ object Implicits extends App {
     println("5! = " + (5!))
     println("0! = " + (0!))
     println("10! = " + (10!))
+    println()
+  }
+
+  def exercise05_implicitConversionsWithImplicitParameters() = {
+    val result = smaller(Fraction(1, 7), Fraction(2, 9))
+
+    println("EXERCISE 5: Implicit conversions with implicit parameters")
+    println(s"smaller(Fraction(1, 7), Fraction(2, 9)) = $result")
     println()
   }
 }
--- src/com/czeczotka/scala/impatient/chapter21/exercise/Fraction.scala
+++ src/com/czeczotka/scala/impatient/chapter21/exercise/Fraction.scala
@@ -10,4 +10,15 @@ object Fraction {
 
   implicit def int2Fraction(n: Int): Fraction = Fraction(n, 1)
 
+  implicit val order: Fraction => Ordered[Fraction] = (fraction: Fraction) => new Ordered[Fraction] {
+    override def compare(that: Fraction): Int = {
+      val result = (fraction.n.toDouble / fraction.m) - (that.n.toDouble / that.m)
+      if (result == 0) 0
+      else if (result > 0) 1
+      else -1
+    }
+  }
+
+  def smaller(f1: Fraction, f2: Fraction)(implicit order: Fraction => Ordered[Fraction]) = if (order(f1) < f2) f1 else f2
+
 }
